73edede931161c2e3482decacd7a35156d78b8cd
Simon Timms <stimms@gmail.com>
2018-12-14 19:01:16 -0700

    Day 5 part 1

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -10,25 +10,18 @@ namespace Day5
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("input.txt").Select(x=>Int32.Parse(x));
-            var sum = 0;
-            foreach (var line in lines)
+            var lines = File.ReadAllLines("input.txt").Select(x => Int32.Parse(x)).ToList();
+            var index = 0;
+            int i = 0;
+            while (index < lines.Count() && index >= 0)
             {
-                var numbers = line.Split("\t").Select(x => Int32.Parse(x));
-                sum += numbers.Max() - numbers.Min();
-            }
-            Console.WriteLine(sum);
-
-            sum = 0;
-            foreach (var line in lines)
-            {
-                var numbers = line.Split("\t").Select(x => Int32.Parse(x)).ToList();
-                var combined = numbers.AllCombinations(numbers);
-                var even = combined.Where(x => x.Item1 % x.Item2 == 0 && x.Item1 != x.Item2).Single();
-                sum += even.Item1 / even.Item2;
+                var newIndex = index + lines[index];
+                lines[index]++;
+                i++;
+                index = newIndex;
             }
 
-            Console.WriteLine(sum);
+            Console.WriteLine(i);
             Console.ReadLine();
         }
     }
