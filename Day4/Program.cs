1400173a98a521aac262dee4a79cc059a81c3212
Simon Timms <stimms@gmail.com>
2018-12-14 17:13:19 -0700

    Adding day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Day4
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+            var valid = 0;
+            foreach (var line in lines)
+            {
+                var words = line.Split(' ');
+                if (words.Distinct().Count() == words.Count())
+                    valid++;
+            }
+            Console.WriteLine(valid);
+            foreach (var line in lines)
+            {
+                var sortedWords = line.Split(' ').Select(x => new String(x.OrderBy(y => y).ToArray()));
+                if (sortedWords.Distinct().Count() == sortedWords.Count())
+                    valid++;
+            }
+            Console.WriteLine(valid);
+            Console.ReadLine();
+        }
+    }
+}
